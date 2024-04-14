bf4cc50d838c37510e02d28ce3e29691c9b2e25e
Murtada Ahmed <murtadhabg@gmail.com>
2024-04-14 18:01:16 +0300

    added handblars and database

--- cheat-sheet.md
+++ cheat-sheet.md
@@ -6,7 +6,7 @@
 
 3. create index.js file
 
-4. install and configure express
+4. express
 >> npm install express
 * start initial express server
 * Add static middleware
@@ -14,7 +14,20 @@
 * Add body parser
 * create routes module
 
-5. Install & config handlebars
+5. handlebars
 * Install express-handlebars
 >> npm install express-handlebars
 * Config handlebars with express
+* Add views folder with resources
+* Add home page
+* Add main layout
+* Add partials dir
+
+6. Database
+* Install mongose
+>> npm install mongoose
+* Setup DB Connection
+* Add user model
+
+
+
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const handlebars = require("express-handlebars");
+const mongoose = require("mongoose");
+
 const routes = require("./routes")
 
 const app = express();
@@ -8,6 +10,16 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: false }));
 
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+
+app.set("view engine", "hbs");
+
 app.use(routes)
 
-app.listen("5000", () => "App is running on http://localhost:5000/") 
+mongoose.connect("mongodb://localhost:27017/course-book")
+
+mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
+mongoose.connection.on("disconnected", () => console.log("Disconnected from MongoDB"));
+mongoose.connection.on("error", (error) => console.log(error));
+ 
+app.listen("5000", () => console.log("App is running on http://localhost:5000/"));
